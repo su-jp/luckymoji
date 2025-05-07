9d43540cc77a64e8887e424fa458daf6af3052ff
Sujin <su_jp@naver.com>
2025-05-07 12:11:10 +0900

    Initial commit

---
+++ src/main/java/com/luckymoji/LuckymojiApplication.java
@@ -0,0 +1,13 @@
+package com.luckymoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckymojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckymojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckymoji/LuckymojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckymoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckymojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
